26a8bc7496335d618847617e6897d7c8469c4293
Malte Wirz <mail@maltewirz.com>
2019-06-11 17:02:32 +0200

    clickhandler implemented

--- index.js
+++ index.js
@@ -206,16 +206,12 @@ app.get('/getFriends/:otherId', async (req, res) => {
     let resp = rows[0];
     console.log("rows", resp);
     if (resp.accepted) {
-        console.log("Unfriend");
         res.json({button: "Unfriend"});
     } else if (resp.sender_id == myId && resp.receiver_id == otherId) {
-        console.log("Cancel Friend Request");
         res.json({button: "Cancel Friend Request"});
     } else if (resp.sender_id == otherId && resp.receiver_id == myId) {
-        console.log("Accept Friend Request");
         res.json({button: "Accept Friend Request"});
     } else {
-        console.log("Send Friend Request");
         res.json({button: "Send Friend Request"});
     }
 });
--- src/friendbutton.js
+++ src/friendbutton.js
@@ -4,20 +4,28 @@ import axios from './axios';
 
 export function FriendButton(props) {
     const [buttonMsg, setButtonMsg] = useState("");
+    const [post, setPost] = useState("");
     // console.log("props in FriendButton",props.myId, props.otherId);
     let otherId = props.otherId;
+    console.log("post", post);
 
     useEffect(() => {
         (async () => {
-            if (otherId != undefined) {
+            console.log("before if ", otherId);
+            if (otherId != undefined ) {
+                console.log("i'm here");
                 let { data } = await axios.get(`/getFriends/${otherId}`);
-                console.log(data);
                 setButtonMsg(data.button);
             }
         })();
-    });
+    },[otherId]);
+
+    function clickHandler(buttonMsg){
+        setPost(buttonMsg);
+
+    }
 
     return(
-        <button className="friendReq">{buttonMsg}</button>
+        <button onClick={e => clickHandler(buttonMsg)}>{buttonMsg}</button>
     );
 }
