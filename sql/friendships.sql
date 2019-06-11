401fb8b442190cd02b6e6800da7b3e2cd6724573
Malte Wirz <mail@maltewirz.com>
2019-06-11 11:47:06 +0200

    test wip

--- sql/friendships.sql
+++ sql/friendships.sql
@@ -1,6 +1,8 @@
+DROP TABLE IF EXISTS friendships;
+
 CREATE TABLE friendships(
     id SERIAL PRIMARY KEY,
     sender_id INTEGER REFERENCES users(id) NOT NULL,
     receiver_id INTEGER REFERENCES users(id) NOT NULL,
-    accepted BOOLEAN DEFAULT false NOT NULL
-)
+    accepted BOOLEAN DEFAULT FALSE NOT NULL
+);
