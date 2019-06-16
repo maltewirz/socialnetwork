7bad5ad34762920ce59ce1d967a4a2ae2c51fb61
Malte Wirz <mail@maltewirz.com>
2019-06-16 12:30:39 +0200

    refactoring from class to function for uploader

--- src/uploader.js
+++ src/uploader.js
@@ -2,33 +2,27 @@ import React from "react";
 import axios from "./axios";
 
 
-export class Uploader extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state= {};
-    }
+export function Uploader(props) {
 
-    async handleInput({ target }) {
+    async function handleInput({ target }) {
         var formData = new FormData();
         formData.append("file",target.files[0]); //formdata to wrap "file"
         try{
             let resp = await axios.post("/upload", formData);
-            this.props.changeImage(resp.data.url);
+            props.changeImage(resp.data.url);
         } catch(err) {
             console.log("err in axios post /upload", err);
         }
     }
 
-    render() {
-        return (
-            <div className="modalWrapper">
-                <div className="modal">
-                    <p id="closeModal" onClick={() => this.props.closeModal()}> X </p>
-                    <p> Want to change your image? </p>
-                    <input type="file" id="file" name="file" accept="image/*" className="inputfield" onChange={e => this.handleInput(e)}/>
-                    <label htmlFor="file" className="button" >Upload</label>
-                </div>
+    return (
+        <div className="modalWrapper">
+            <div className="modal">
+                <p id="closeModal" onClick={() => props.closeModal()}> X </p>
+                <p> Want to change your image? </p>
+                <input type="file" id="file" name="file" accept="image/*" className="inputfield" onChange={e => handleInput(e)}/>
+                <label htmlFor="file" className="button" >Upload</label>
             </div>
-        );
-    }
+        </div>
+    );
 }
