2e78a4c8eb41110128c990522d55502c8128c4f7
Malte Wirz <mail@maltewirz.com>
2019-06-16 12:00:01 +0200

    refactoring from class to function for logo

--- src/logo.js
+++ src/logo.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 
-export class Logo extends React.Component {
-    render() {
-        return (
-            <Link to="/">
-                <img className="logoPng" src="../logo.png" />
-            </Link>
-        );
-    }
+export function Logo() {
+    return (
+        <Link to="/">
+            <img className="logoPng" src="../logo.png" />
+        </Link>
+    );
 }
